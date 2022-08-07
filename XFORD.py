606cc797f338a4c87584529dcf6de6580dca77e3
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-08-07 18:32:42 +0100

    .

--- XFORD.py
+++ XFORD.py
@@ -25,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("XFORD").main_chi()
+		__import__("XFORD").login_()
 	else:
-		__import__("XFORD").main_chi()
+		__import__("XFORD").login_()
